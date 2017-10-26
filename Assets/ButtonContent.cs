e16f3023c19d3196bccf743866c0102cec6feb42
Unknown <jjb672@nyu.edu>
2017-10-26 16:10:55 -0400

    Added buttons that shance size depending on content.

---
+++ Assets/ButtonContent.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonContent : MonoBehaviour {
+
+    public Text buttonContent;
+
+	// Use this for initialization
+	void Start () {
+		if(buttonContent == null)
+        {
+            Debug.LogError("There is no button text reference!");
+        }
+        else
+        {
+            buttonContent.transform.parent.GetComponent<RectTransform>();
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
